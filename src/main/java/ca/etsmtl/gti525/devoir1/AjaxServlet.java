52a517abd8c898a11d3a78b625da98624630402d
Elisabld <elisabld21@gmail.com>
2017-02-27 20:45:42 -0500

    Update AjaxServlet.java

--- src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
+++ src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
@@ -1,1 +1,24 @@
+ package ca.etsmtl.gti525.devoir1;
+
+ import java.io.* ;
+ import java.text.* ;
+ import java.util.* ;
+ import javax.servlet.* ;
+ import javax.servlet.http.* ;
+
+ public Web  extends HttpServlet {
+
+	 public  void doGet(HttpServletRequest request, HttpServletResponse response)
+	 throws ServletException, IOException  {
+	
+     Image image = (Image)request.getAttribute("image");
+
+	}
+
+	 public  void doPost(HttpServletRequest request, HttpServletResponse response)
+	 throws ServletException, IOException  {
+	
+		doGet(request, response) ;
+	}
+}
 
