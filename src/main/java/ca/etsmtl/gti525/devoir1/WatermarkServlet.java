b1b6f544fe49e8512df3b4973c72972fec0a5442
Elisabld <elisabld21@gmail.com>
2017-03-03 13:16:38 -0500

    Update WatermarkServlet.java

--- src/main/java/ca/etsmtl/gti525/devoir1/WatermarkServlet.java
+++ src/main/java/ca/etsmtl/gti525/devoir1/WatermarkServlet.java
@@ -25,8 +25,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.ImageIcon;
 
-@WebServlet("/watermark")
-
 public class WatermarkServlet extends HttpServlet {
 	private static final long serialVersionUID = -4865099303373864287L;
 
